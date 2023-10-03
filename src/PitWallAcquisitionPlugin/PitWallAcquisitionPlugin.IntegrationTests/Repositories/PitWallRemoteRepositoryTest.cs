11a166fc6e78dcd8971da3b9c49176a8a685612b
MacReiben <macreiben@gmail.com>
2023-10-03 16:30:24 +0200

    code cleanup.

--- src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.IntegrationTests/Repositories/PitWallRemoteRepositoryTest.cs
+++ src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.IntegrationTests/Repositories/PitWallRemoteRepositoryTest.cs
@@ -1,11 +1,6 @@
 ﻿using NFluent;
 using PitWallAcquisitionPlugin.Aggregations;
 using PitWallAcquisitionPlugin.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PitWallAcquisitionPlugin.IntegrationTests.Repositories
 {
