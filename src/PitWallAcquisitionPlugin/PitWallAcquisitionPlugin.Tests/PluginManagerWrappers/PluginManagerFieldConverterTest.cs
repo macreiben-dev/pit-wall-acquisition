8529c2348c6fcdf3d83dfca694b13b96c7f94c07
MacReiben <macreiben@gmail.com>
2023-10-03 03:23:58 +0200

    plugin manager conversion method covered using test after.

--- src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.Tests/PluginManagerWrappers/PluginManagerFieldConverterTest.cs
+++ src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.Tests/PluginManagerWrappers/PluginManagerFieldConverterTest.cs
@@ -33,5 +33,35 @@ namespace PitWallAcquisitionPlugin.Tests.PluginManagerWrappers
 
             Check.That(actual).IsEqualTo("data_value");
         }
+
+        [Fact]
+        public void GIVEN_pluginManager_return_null_AND_toDouble_invoked_THEN_return_null()
+        {
+            _pluginManager.GetPropertyValue("some_key").Returns((double?)null);
+
+            var actual = PluginManagerFieldConverter.ToDouble("some_key", _pluginManager);
+
+            Check.That(actual).IsNull();
+        }
+
+        [Fact]
+        public void GIVEN_pluginManager_return_data_AND_toDouble_invoked_THEN_return_data()
+        {
+            _pluginManager.GetPropertyValue("some_key").Returns(23.325515);
+
+            var actual = PluginManagerFieldConverter.ToDouble("some_key", _pluginManager);
+
+            Check.That(actual).IsEqualTo(23.325515);
+        }
+
+        [Fact]
+        public void GIVEN_pluginManager_return_null_AND_toBoolean_invoked_THEN_return_false()
+        {
+            _pluginManager.GetPropertyValue("some_key").Returns((bool?)null);
+
+            var actual = PluginManagerFieldConverter.ToBoolean("some_key", _pluginManager);
+
+            Check.That(actual).IsFalse();
+        }
     }
 }
