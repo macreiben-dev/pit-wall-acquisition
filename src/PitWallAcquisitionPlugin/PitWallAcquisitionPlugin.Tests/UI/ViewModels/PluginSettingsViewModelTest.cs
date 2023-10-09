cd34f49ba4224539b138c0ea76079778e325e320
MacReiben <macreiben@gmail.com>
2023-10-10 00:01:28 +0200

    added api address

--- src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.Tests/UI/ViewModels/PluginSettingsViewModelTest.cs
+++ src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.Tests/UI/ViewModels/PluginSettingsViewModelTest.cs
@@ -7,8 +7,11 @@ namespace PitWallAcquisitionPlugin.Tests.UI.ViewModels
 {
     public class PluginSettingsViewModelTest
     {
+        private const string VALID_API_ADDRESS = "http://api.address.net";
+
         [Fact]
-        public void GIVEN_pilotName_isSet_THEN_notifyPropertychanged_pilotName() {
+        public void GIVEN_pilotName_isSet_THEN_notifyPropertychanged_pilotName()
+        {
 
             var target = new PluginSettingsViewModel();
 
@@ -44,6 +47,73 @@ namespace PitWallAcquisitionPlugin.Tests.UI.ViewModels
             target.PilotName = input;
 
             Check.That(target["PilotName"]).IsEqualTo("Pilot name must be set.");
+        }
+
+        [Fact]
+        public void GIVEN_ApiAddress_isSet_THEN_notifyPropertychanged_ApiAdress()
+        {
+            var target = new PluginSettingsViewModel();
+
+            using var monitored = target.Monitor();
+
+            target.ApiAddress = VALID_API_ADDRESS;
+
+            monitored.Should().Raise("PropertyChanged")
+                .WithSender(target)
+                .WithArgs<PropertyChangedEventArgs>(
+                    e => e.PropertyName == "ApiAddress");
+        }
+
+        [Fact]
+        public void GIVEN_ApiAddress_isSet_THEN_ApiAddress_hasExpectedValue()
+        {
+            var target = new PluginSettingsViewModel();
+
+            target.ApiAddress = VALID_API_ADDRESS;
+
+            Check.That(target.ApiAddress).IsEqualTo(VALID_API_ADDRESS);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData(" ")]
+        [InlineData("      ")]
+        public void GIVEN_ApiAddress_isNotSet_THEN_dataErrorInfo_returns_notSet(string input)
+        {
+            var target = new PluginSettingsViewModel();
+
+            target.ApiAddress = input;
+
+            Check.That(target["ApiAddress"]).IsEqualTo("API address must be set.");
+        }
+
+        [Fact]
+        public void GIVEN_apiAddress_uri_isValid_THEN_error_isNull()
+        {
+            var target = new PluginSettingsViewModel();
+
+            target.ApiAddress = VALID_API_ADDRESS;
+
+            var actual = target["ApiAddress"];
+
+            Check.That(actual).IsNull();
+        }
+
+        [Theory]
+        [InlineData("htttttp://....ext")]
+        [InlineData("http://test,test2.ext")]
+        [InlineData("http://test=test2.ext")]
+        public void GIVEN_apiAddress_uri_isNotValid_THEN_error_isSet(string input)
+        {
+            var target = new PluginSettingsViewModel();
+
+            target.ApiAddress = input;
+
+            var actual = target["ApiAddress"];
+
+            Check.That(actual).IsEqualTo(
+                "API URI format is invalid. Should look like http://domain.ext or http://domain.ext");
         }
     }
 }
--- src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin/UI/ViewModels/PluginSettingsViewModel.cs
+++ src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin/UI/ViewModels/PluginSettingsViewModel.cs
@@ -6,7 +6,11 @@ namespace PitWallAcquisitionPlugin.UI.ViewModels
     public class PluginSettingsViewModel : INotifyPropertyChanged, IDataErrorInfo
     {
         private const string PILOTNAME_MUST_BE_SET = "Pilot name must be set.";
+        private const string VALIDATION_APIADDRESS_MUST_BE_SET = "API address must be set.";
+        private const string VALIDATION_APIADDRESS_URI_FORMAT = "API URI format is invalid. Should look like http://domain.ext or http://domain.ext";
+
         private string _pilotName;
+        private string _apiAddress;
 
         public string PilotName
         {
@@ -18,21 +22,51 @@ namespace PitWallAcquisitionPlugin.UI.ViewModels
             }
         }
 
+        public string ApiAddress
+        {
+            get => _apiAddress;
+            set
+            {
+                _apiAddress = value;
+                NotifyPropertyChanged(nameof(ApiAddress));
+            }
+        }
+
         #region Observability
 
         public string this[string propertyName]
         {
             get
             {
-                SimHub.Logging.Current.Info($"DataErrorInfo called [{propertyName}]");
-
                 switch (propertyName)
                 {
                     case nameof(PilotName):
-                        if (string.IsNullOrEmpty(PilotName) || 
+                        if (string.IsNullOrEmpty(PilotName) ||
                             string.IsNullOrWhiteSpace(PilotName))
                         {
                             return PILOTNAME_MUST_BE_SET;
+                        }
+                        break;
+                    case nameof(ApiAddress):
+                        if (string.IsNullOrEmpty(ApiAddress) ||
+                            string.IsNullOrWhiteSpace(ApiAddress))
+                        {
+                            return VALIDATION_APIADDRESS_MUST_BE_SET;
+                        }
+
+                        Uri convetedUri = null;
+
+                        var isFormatValid = Uri.TryCreate(
+                            ApiAddress, 
+                            UriKind.Absolute,        
+                            out convetedUri)
+                            && convetedUri != null && (
+                                convetedUri.Scheme == Uri.UriSchemeHttp 
+                                || convetedUri.Scheme == Uri.UriSchemeHttps);
+
+                        if (!isFormatValid)
+                        {
+                            return VALIDATION_APIADDRESS_URI_FORMAT;
                         }
                         break;
                 }
