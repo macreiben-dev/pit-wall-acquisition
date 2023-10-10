1841a914a5cefbb521359e17c52b7b94f8701dfa
MacReiben <macreiben@gmail.com>
2023-10-10 19:26:40 +0200

    added post build event to deploy and run the plugin in simhub folder. ViewModel set to DataContext from loaded event to ensure form's field state is ok.

--- src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin/UI/Views/PluginSettings.xaml.cs
+++ src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin/UI/Views/PluginSettings.xaml.cs
@@ -12,6 +12,22 @@ namespace PitWallAcquisitionPlugin.UI.Views
         {
             InitializeComponent();
 
+            this.Loaded += PluginSettings_Loaded;
+
+           
+        }
+
+        private void PluginSettings_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            /**
+             * Idea : load configuration from saved configuration.
+             * Should create inject an implementation of configuration repository to do the heavy lifting.
+             * */
+
+            /**
+             * Idea: consider using an ioc engine to create what needs to be created. 
+             * Eventualy pass reference on plugin to add a Start/Stop button also.
+             * */
             DataContext = new PluginSettingsViewModel();
         }
     }
