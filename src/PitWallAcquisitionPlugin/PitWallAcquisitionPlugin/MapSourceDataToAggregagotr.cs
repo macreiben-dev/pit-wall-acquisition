8c1852d71be4c55e6529d5a88cc4cb50f74b0d30
MacReiben <macreiben@gmail.com>
2023-10-29 17:06:12 +0100

    wip integration configuration.

--- src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin/MapSourceDataToAggregagotr.cs
+++ src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin/MapSourceDataToAggregagotr.cs
@@ -1,5 +1,6 @@
 ﻿using FuelAssistantMobile.DataGathering.SimhubPlugin;
 using PitWallAcquisitionPlugin.Aggregations;
+using PitWallAcquisitionPlugin.Aggregations.v2;
 
 namespace PitWallAcquisitionPlugin
 {
@@ -7,24 +8,42 @@ namespace PitWallAcquisitionPlugin
     {
         public static void UpdateAggregatorNow(ILiveAggregator aggregator, IPluginRecordRepository racingDataRepository)
         {
+            var allConfiguration = new[]
+            {
+                new LiveMapper(r => r.AirTemperature, (a, data) => a.SetAirTemperature(data)),
+                new LiveMapper(r => r.AvgRoadWetness, (a, data) => a.SetAvgWetness(data)),
+
+                // ----
+
+                new LiveMapper(r => r.TyreFrontLeftTemperature.Average, (a, data) => a.SetFrontLeftTyreTemperature(data)),
+                new LiveMapper(r => r.TyreRearLeftTemperature.Average, (a, data) => a.SetRearLeftTyreTemperature(data)),
+                new LiveMapper(r => r.TyreFrontRightTemperature.Average, (a, data) => a.SetFrontRightTyreTemperature(data)),
+                //new LiveMapper(r => r.TyreFrontLeftTemperature.Average, (a, data) => a.SetFrontLeftTyreTemperature(data)),
+            };
+
+            foreach (var config in allConfiguration)
+            {
+                config.Set(racingDataRepository, aggregator);
+            }
+
             aggregator.SetSessionTimeLeft(racingDataRepository.SessionTimeLeft);
 
             aggregator.SetLaptime(racingDataRepository.LastLaptime);
 
-            aggregator.SetAirTemperature(racingDataRepository.AirTemperature);
+            //aggregator.SetAirTemperature(racingDataRepository.AirTemperature);
 
-            aggregator.SetAvgWetness(racingDataRepository.AvgRoadWetness);
+            //aggregator.SetAvgWetness(racingDataRepository.AvgRoadWetness);
 
             aggregator.SetFrontLeftTyreWear(racingDataRepository.TyreWearFrontLeft);
             aggregator.SetFrontRightTyreWear(racingDataRepository.TyreWearFrontRight);
             aggregator.SetRearLeftTyreWear(racingDataRepository.TyreWearRearLeft);
             aggregator.SetRearRightTyreWear(racingDataRepository.TyreWearRearRight);
 
-            aggregator.SetFrontLeftTyreTemperature(racingDataRepository.TyreFrontLeftTemperature.Average);
+            //aggregator.SetFrontLeftTyreTemperature(racingDataRepository.TyreFrontLeftTemperature.Average);
 
-            aggregator.SetFrontRightTyreTemperature(racingDataRepository.TyreFrontRightTemperature.Average);
+            //aggregator.SetFrontRightTyreTemperature(racingDataRepository.TyreFrontRightTemperature.Average);
 
-            aggregator.SetRearLeftTyreTemperature(racingDataRepository.TyreRearLeftTemperature.Average);
+            //aggregator.SetRearLeftTyreTemperature(racingDataRepository.TyreRearLeftTemperature.Average);
 
             aggregator.SetRearRightTyreTemperature(racingDataRepository.TyreRearRightTemperature.Average);
         }
