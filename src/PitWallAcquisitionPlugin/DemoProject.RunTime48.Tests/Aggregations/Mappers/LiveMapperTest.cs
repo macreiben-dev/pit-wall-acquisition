f2c2302dcedcb2e0ddd19435cf3c622f440c2d7a
MacReiben <macreiben@gmail.com>
2024-01-10 10:39:21 +0100

    testing with reference on plugin - detected a reference to integration tests from unit tests project. Could be the issue since the integration tests is in .Net 6.

---
+++ src/PitWallAcquisitionPlugin/DemoProject.RunTime48.Tests/Aggregations/Mappers/LiveMapperTest.cs
@@ -0,0 +1,77 @@
+﻿using FuelAssistantMobile.DataGathering.SimhubPlugin;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using NSubstitute;
+using PitWallAcquisitionPlugin.Aggregations.Telemetries.Aggregators;
+using PitWallAcquisitionPlugin.Aggregations.Telemetries.Mappers;
+using System;
+
+namespace PitWallAcquisitionPlugin.RunTime48.Tests.Aggregations.Mappers
+{
+    public class LiveMapperTest
+    {
+        private ITelemetryLiveAggregator _aggregator;
+        private IPluginRecordRepository _adapter;
+        private Func<IPluginRecordRepository, string> _sourceSelector;
+        private Action<ITelemetryLiveAggregator, string> _setter;
+
+        public LiveMapperTest() {
+            _aggregator = Substitute.For<ITelemetryLiveAggregator>();
+            _adapter = Substitute.For<IPluginRecordRepository>();
+
+            Func<IPluginRecordRepository, string> sourceSelector = (r) => r.LastLaptime;
+
+            Action<ITelemetryLiveAggregator, string> setter = (a, counter) => a.SetLaptime(counter);
+
+            _sourceSelector = sourceSelector;
+
+            _setter = setter;
+        }
+
+        [TestMethod]
+        public void WHEN_sourceSelector_isNull_THEN_fail()
+        {
+            Check.ThatCode(() => new LiveTelemetryMapper<string>(null, _setter))
+                .Throws<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void WHEN_setter_isNull_THEN_fail()
+        {
+            Check.ThatCode(() => new LiveTelemetryMapper<string>(_sourceSelector, null))
+                .Throws<ArgumentNullException>();
+        }
+
+        [TestMethod]
+        public void WHEN_adapter_isNull_THEN_fail()
+        {
+            Func<IPluginRecordRepository, string> sourceSelector = (r) => r.LastLaptime;
+
+            Action<ITelemetryLiveAggregator, string> setter = (a, counter) => a.SetLaptime(counter);
+
+            var target = new LiveTelemetryMapper<string>(
+                    sourceSelector,
+                    setter
+                );
+
+            Check.ThatCode(() => target.Set(null, _aggregator))
+                .Throws<ArgumentNullException>();   
+        }
+
+        [TestMethod]
+        public void WHEN_aggregator_isNull_THEN_fail()
+        {
+            Func<IPluginRecordRepository, string> sourceSelector = (r) => r.LastLaptime;
+
+            Action<ITelemetryLiveAggregator, string> setter = (a, counter) => a.SetLaptime(counter);
+
+            var target = new LiveTelemetryMapper<string>(
+                    sourceSelector,
+                    setter
+                );
+
+            Check.ThatCode(() => target.Set(_adapter, null))
+                .Throws<ArgumentNullException>();
+        }
+    }
+}
