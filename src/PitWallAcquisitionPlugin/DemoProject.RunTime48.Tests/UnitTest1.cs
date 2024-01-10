dd3bcf9ea2396e4c7892aa3a7834967900e0aae9
MacReiben <macreiben@gmail.com>
2024-01-10 10:09:31 +0100

    created a new test assembly to make unittesst work again.

---
+++ src/PitWallAcquisitionPlugin/DemoProject.RunTime48.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+using System;
+
+namespace DemoProject.RunTime48.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GIVEN_something_THEN_fail()
+        {
+            Check.That(1).IsEqualTo(2);
+        }
+    }
+}
