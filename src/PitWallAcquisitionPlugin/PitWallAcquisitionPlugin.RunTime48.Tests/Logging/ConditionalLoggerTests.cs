a7470c6b36ebed9d5626e117ca9a067cbbd4c356
Christian Finel <macreiben@gmail.com>
2024-06-03 18:13:05 +0200

    conditional logger implementation created.

--- src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.RunTime48.Tests/Logging/ConditionalLoggerTests.cs
+++ src/PitWallAcquisitionPlugin/PitWallAcquisitionPlugin.RunTime48.Tests/Logging/ConditionalLoggerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using NFluent;
+using PitWallAcquisitionPlugin.Logging;
 using Xunit;
 
 namespace PitWallAcquisitionPlugin.RunTime48.Tests.Logging
@@ -8,8 +9,10 @@ namespace PitWallAcquisitionPlugin.RunTime48.Tests.Logging
     {
         private readonly Action<string> _logger;
         private string _actualMessage;
+        private string _repeatedMessage;
         private int _invokeCount;
         private const string OriginalMessage = "Some original message";
+        private const string PreviousMessageWasTheSameAsTheCurrentOneSkipping = "Previous message was the same as the current one. Skipping.";
 
         public ConditionalLoggerTests()
         {
@@ -27,19 +30,6 @@ namespace PitWallAcquisitionPlugin.RunTime48.Tests.Logging
         {
             return new ConditionalLogger();
         }
-        
-        [Fact]
-        public void GIVEN_message_is_same_THEN_do_not_invoke()
-        {
-            // ACT
-            var target = new ConditionalLogger();
-
-            target.Info(OriginalMessage, _logger);
-            target.Info(OriginalMessage, _logger);
-
-            // ASSERT
-            Check.That(_invokeCount).IsEqualTo(1);
-        }
 
         [Fact]
         public void GIVEN_invoke_once_THEN_invoke()
@@ -49,7 +39,7 @@ namespace PitWallAcquisitionPlugin.RunTime48.Tests.Logging
             // ACT
             var target = new ConditionalLogger();
 
-            target.Info(OriginalMessage, _logger);
+            target.Log(OriginalMessage, _logger);
 
             // ASSERT
             Check.That(_invokeCount).IsEqualTo(1);
@@ -64,8 +54,8 @@ namespace PitWallAcquisitionPlugin.RunTime48.Tests.Logging
             // ACT
             var target = GetTarget();
 
-            target.Info(OriginalMessage, _logger);
-            target.Info(OriginalMessage + "02", _logger);
+            target.Log(OriginalMessage, _logger);
+            target.Log(OriginalMessage + "02", _logger);
 
             // ASSERT
             Check.That(_invokeCount).IsEqualTo(2);
@@ -84,10 +74,10 @@ namespace PitWallAcquisitionPlugin.RunTime48.Tests.Logging
             // ACT
             var target = GetTarget();
 
-            target.Info(originalMessage01, _logger);
+            target.Log(originalMessage01, _logger);
             actualMessage01 = _actualMessage;
             
-            target.Info(originalMessage02, _logger);
+            target.Log(originalMessage02, _logger);
             actualMessage02 = _actualMessage;
 
             // ASSERT
@@ -95,5 +85,67 @@ namespace PitWallAcquisitionPlugin.RunTime48.Tests.Logging
             Check.That(actualMessage02).IsEqualTo(originalMessage02);
         }
 
+        [Fact]
+        public void GIVEN_message_is_repeated_twice_THEN_log_repeatedMessage_warn()
+        {
+            // ACT
+            var target = GetTarget();
+            
+            target.Log(OriginalMessage, _logger);
+            target.Log(OriginalMessage, _logger);
+
+            // ASSERT
+            Check.That(_actualMessage).IsEqualTo(PreviousMessageWasTheSameAsTheCurrentOneSkipping);
+        }
+
+        [Fact]
+        public void GIVEN_message_repeated_threetimes_THEN_log_message_one_AND_repeatedMessace_once()
+        {
+            // ACT
+            var target = GetTarget();
+            
+            target.Log(OriginalMessage, _logger);
+            
+            Check.That(_actualMessage).IsEqualTo(OriginalMessage);
+            
+            target.Log(OriginalMessage, _logger);
+            
+            Check.That(_actualMessage).IsEqualTo(PreviousMessageWasTheSameAsTheCurrentOneSkipping);
+
+            target.Log(OriginalMessage, _logger);
+
+            Check.That(_invokeCount).IsEqualTo(2);
+        }
+
+        [Fact]
+        public void GIVEN_three_different_messages_THEN_log_messages()
+        {
+            // ARRANGE
+            string actualMessage01 = null;
+            string actualMessage02 = null;
+            string actualMessage03 = null;
+
+            var originalMessage01 = OriginalMessage;
+            var originalMessage02 = OriginalMessage + "02";
+            var originalMessage03 = OriginalMessage + "03";
+            
+            // ACT
+            var target = GetTarget();
+
+            target.Log(originalMessage01, _logger);
+            actualMessage01 = _actualMessage;
+            
+            target.Log(originalMessage02, _logger);
+            actualMessage02 = _actualMessage;
+            
+            target.Log(originalMessage03, _logger);
+            actualMessage03 = _actualMessage;
+
+            // ASSERT
+            Check.That(actualMessage01).IsEqualTo(originalMessage01);
+            Check.That(actualMessage02).IsEqualTo(originalMessage02);
+            Check.That(actualMessage03).IsEqualTo(originalMessage03);
+
+        }
     }
 }
